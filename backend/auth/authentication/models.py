0676726f2d2a97492a5f4108d1a619f1b697132c
Elodie Richardeau <elrichar@student.42.fr>
2024-09-27 15:39:45 +0200

    feat: create auth model

--- backend/auth/authentication/models.py
+++ backend/auth/authentication/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 
-# Create your models here.
+class User(AbstractUser):
+	email = models.EmailField(unique=True)
+	avatar = models.URLField(blank=True, null=True)
+	is_active = models.BooleanField(default=True)
+	created_at = models.DateTimeField(default=timezone.now)
+
+	groups = models.ManyToManyField(
+		'auth.Group',
+		related_name='custom_user_set'
+	)
+
+	user_permissions = models.ManyToManyField(
+		'auth.Permission',
+		related_name='custom_user_permissions_set',
+	)
+
+	def __str__(self):
+		return self.username
+
+	class Meta:
+		verbose_name = 'User'
+		verbose_name_plural = 'Users'
