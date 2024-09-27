8a814a39d0097a217e4f79717b001e6159329a19
Andrew Attali <me@mortebrume.eu>
2024-09-27 15:54:30 +0200

    feat: finished base User model

--- backend/auth/authentication/models.py
+++ backend/auth/authentication/models.py
@@ -1,26 +1,26 @@
+from django.core.validators import MinLengthValidator
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
+from validators import validate_alnum
 
 class User(AbstractUser):
+	username = models.CharField(max_length=15, unique=True, validators=[MinLengthValidator(3), validate_alnum])
+	nickname = models.CharField(max_length=30)
 	email = models.EmailField(unique=True)
 	avatar = models.URLField(blank=True, null=True)
 	is_active = models.BooleanField(default=True)
 	created_at = models.DateTimeField(default=timezone.now)
+	friends = models.ForeignKey("self", related_name='friends', on_delete=models.SET_NULL)
 
-	groups = models.ManyToManyField(
-		'auth.Group',
-		related_name='custom_user_set'
-	)
+	USERNAME_FIELD = username
+	REQUIRED_FIELDS = ['email', 'nickname']
 
-	user_permissions = models.ManyToManyField(
-		'auth.Permission',
-		related_name='custom_user_permissions_set',
-	)
+	# TODO: Model generation of user to do via manager later
+	# TODO: objects = ManagerToBeDone()
 
 	def __str__(self):
 		return self.username
 
-	class Meta:
-		verbose_name = 'User'
-		verbose_name_plural = 'Users'
+	def get_full_name(self):
+		return self.nickname
