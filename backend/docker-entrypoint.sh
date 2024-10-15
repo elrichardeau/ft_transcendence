caeb84a28f05ae905a136900a422d43b5d7f46ee
Andrew Attali <me@mortebrume.eu>
2024-10-15 09:14:04 +0200

    fix: create database migrations before launching the apps

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -5,6 +5,10 @@ echo "Starting Django..."
 
 . "/generate-ssl-certificate.sh"
 
+echo "Making database migrations..."
+python manage.py makemigrations --noinput
+echo "Migrations creation completed."
+
 echo "Applying database migrations..."
 python manage.py migrate --noinput
 echo "Database migrations completed."
