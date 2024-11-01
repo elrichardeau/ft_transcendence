30ec12c0fca467f7d2e8d2af89ea2269c8669c98
elodie <elodie15@gmail.com>
2024-11-01 16:04:10 +0100

    feat: create script for ca trust on windows

--- vault/scripts/remove-ca.sh
+++ vault/scripts/remove-ca.sh
@@ -4,6 +4,10 @@ if [ -f "./vault/ca/transcendence.crt" ]; then
   if [ "$(uname)" == "Darwin" ]; then
     sudo security delete-certificate -c 'transcendence.local' -t /Library/Keychains/System.keychain
     sudo security delete-certificate -c 'TranscendenceLocalIntermediateAuthority' -t /Library/Keychains/System.keychain
+  elif [ "$(uname -a | grep "microsoft")" ]; then
+    gsudo certutil.exe -delstore root 'transcendence.local'
+    gsudo certutil.exe -delstore ca 'transcendence.local'
+    sudo touch ./vault/ca/init
   elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     sudo rm /usr/local/share/ca-certificates/transcendence_ca.crt
     sudo rm /usr/local/share/ca-certificates/transcendence.crt
