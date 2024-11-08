a2d329c1adc1d3bb4f00c0cc748788d0ba7eff04
Hubert-Vecchioli <70095581+Hubert-Vecchioli@users.noreply.github.com>
2024-11-08 17:57:35 +0100

    feat: added pseudo code of the needed APIS

--- backend/chat/livechat/views.py
+++ backend/chat/livechat/views.py
@@ -1,3 +1,80 @@
 from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
 
-# Get has unread messages
+# todo: 2/ When a new message is received while the logged user is not on the chat page
+
+
+class IngestUsers(APIView):
+    # Notice that we received a POST from a succesful user
+    # Trigger a GET User data
+    # Save the user data in our DB (create or update)
+    # Loop over the friend list
+    #   Trigger a GET User data
+    #   Save the user data in our DB (create or update)
+    def get(self, request):
+        return
+
+
+class LiveChatPage(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        # Returns the page html & format
+        return
+
+
+class LiveChatFriends(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        # Returns all the friends of the logged users
+        return
+
+
+class LiveChatConversation(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        # Returns all the conversation of the logged users
+        return
+
+
+class LiveChatMessages(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        # If it exists and not conversation is not blocked
+        #   Method to change the right hasUnreadMessages to FALSE
+        #   Returns all the conversation of the logged users
+        # Create a conversation and returns NULL
+        return
+
+
+class LiveChatBlockUser(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        # If Unblock
+        #   Block and returns 200
+        # Unblock and returns 200
+        return
+
+
+class LiveChatSendMessage(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        # Atomic method
+        # Send the message to web socket ?
+        # Create a new message in the DB
+        return
+
+
+class LiveChatSendInvitation(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        # generate a Pong party link
+        # trigger a post to LiveChatSendMessage
+        return
